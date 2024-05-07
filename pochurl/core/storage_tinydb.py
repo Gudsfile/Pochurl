5c4d8e8443c4af6d16e52fff89cb8cde7e1b4b8c
Gudsfile <gudsfile@protonmail.com>
2024-05-08 01:25:34 +0200

    ♻️  Move code

--- pochurl/core/storage_tinydb.py
+++ pochurl/core/storage_tinydb.py
@@ -1,5 +1,6 @@
 import json
 from datetime import datetime
+from typing import List
 
 from tinydb import TinyDB, Query
 from tinydb.table import Document
@@ -11,32 +12,29 @@ from pochurl.domain import GivenElement, SavedElement
 db = TinyDB('pochurl-tinydb.json')
 
 
-def read_items_by_url(url: AnyHttpUrl):
-    Element = Query()
-    condition = Query().url.test(_test_str_contains, str(url))
-    previous = db.search(condition)
-    return [SavedElement(**(dict(p) | {'id': str(p.doc_id)})) for p in previous] if previous else None
+def read_item(id: str) -> SavedElement | None:
+    previous = db.get(doc_id=id)
+    return SavedElement(id=id, **dict(previous)) if previous else None
 
-def read_items_by_name(name: str):
-    Element = Query()
+def read_items() -> List[SavedElement]:
+    previous = db.all()
+    return [SavedElement(id=str(p.doc_id), **dict(p)) for p in previous] if previous else []
+
+def read_items_by_name(name: str) -> List[SavedElement]:
     condition = Query().name.test(_test_str_contains, name)
     previous = db.search(condition)
-    return [SavedElement(**(dict(p) | {'id': str(p.doc_id)})) for p in previous] if previous else None
+    return [SavedElement(id=str(p.doc_id), **dict(p)) for p in previous] if previous else []
 
-def read_item(id: str):
-    previous = db.get(doc_id=id)
-    return SavedElement(**(dict(previous) | {'id': id})) if previous else None
+def read_items_by_url(url: AnyHttpUrl) -> List[SavedElement]:
+    condition = Query().url.test(_test_str_contains, str(url))
+    previous = db.search(condition)
+    return [SavedElement(id=str(p.doc_id), **dict(p)) for p in previous] if previous else []
 
-def read_items():
-    previous = db.all()
-    print(previous)
-    return [SavedElement(**(dict(p) | {'id': str(p.doc_id)})) for p in previous] if previous else None
-
-def write_item(element: GivenElement):
+def write_item(element: GivenElement) -> str:
     id = db.insert(json.loads(element.json()) | {'timestamp': datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')})
     return str(id)
 
-def rewrite_item(id: str, element: GivenElement):
+def rewrite_item(id: str, element: GivenElement) -> str | None:
     previous = read_item(id)
     if previous:
         db.upsert(Document(json.loads(element.json()), doc_id = int(id)))
