b53993b3ff679097cb124abb34b1f6a3d65bed4c
Gudsfile <gudsfile@protonmail.com>
2024-05-11 01:45:06 +0200

    💄 Init ui

---
+++ pochurl/domain/form.py
@@ -0,0 +1,16 @@
+from typing import Literal
+
+from pydantic import AnyHttpUrl, BaseModel
+
+
+TagList = Literal['👀', '✨', '🚨', '🔥', '🗞️', '🗑️', '🤡', '👻']
+
+
+class GivenElementForm(BaseModel):
+    url: AnyHttpUrl
+    name: str
+    tag: TagList | None = None
+
+
+class TagFilterForm(BaseModel):
+    tag: TagList | None = None
