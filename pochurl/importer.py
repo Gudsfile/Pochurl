a7d26eafa4b38c1de3b3330c23e0b77162211c52
Gudsfile <gudsfile@protonmail.com>
2024-05-12 02:32:21 +0200

    🚧 Parse elements from string

--- pochurl/importer.py
+++ pochurl/importer.py
@@ -1,11 +1,10 @@
+import csv
+
 from pochurl.domain import GivenElement
 
 
 def csv_to_dicts(csv_content: str) -> list:
-    columns = ["url", "name", "tags"]
-    return [
-        dict(((k, {v.strip()}) if k == "tags" else (k, v.strip()) for k, v in zip(columns, line.split(",")))) for line in csv_content.split("\n") if line != ""
-    ]
+    return list(csv.DictReader(csv_content.strip().split("\n"), skipinitialspace=True, fieldnames=["url", "name", "tags"]))
 
 
 def dict_to_elements(dict_content: dict) -> list[GivenElement]:
--- tests/test_importer.py
+++ tests/test_importer.py
@@ -15,9 +15,9 @@ https://example2.py,name2,test
 https://example3.py,name3,python
 """
     intermediate = [
-        {"url": "https://example1.py", "name": "name1", "tags": {"test"}},
-        {"url": "https://example2.py", "name": "name2", "tags": {"test"}},
-        {"url": "https://example3.py", "name": "name3", "tags": {"python"}},
+        {"url": "https://example1.py", "name": "name1", "tags": "test"},
+        {"url": "https://example2.py", "name": "name2", "tags": "test"},
+        {"url": "https://example3.py", "name": "name3", "tags": "python"},
     ]
     assert csv_to_dicts(given) == intermediate
 
@@ -25,9 +25,9 @@ https://example3.py,name3,python
 @pytest.mark.skip(reason="not implemented")
 def test_add_several_items_final():
     intermediate = [
-        {"url": "https://example1.py", "name": "name1", "tags": {"test"}},
-        {"url": "https://example2.py", "name": "name2", "tags": {"test"}},
-        {"url": "https://example3.py", "name": "name3", "tags": {"python"}},
+        {"url": "https://example1.py", "name": "name1", "tags": "test"},
+        {"url": "https://example2.py", "name": "name2", "tags": "test"},
+        {"url": "https://example3.py", "name": "name3", "tags": "python"},
     ]
     expected = [
         GivenElement(url="https://example1.py", name="name1", tags={"test"}),
@@ -41,12 +41,12 @@ def test_add_special_items_intermediate():
     given = """
 "https://example1.py","without_tag",
 "https://example2.py","with space", "with space"
-"https://example3.py","with_several_tags", "one_tag", "two_tag"
+"https://example3.py","with_several_tags","one_tag,two_tag"
 """
     intermediate = [
-        {"url": "https://example1.py", "name": "without_tag", "tags": set()},
-        {"url": "https://example2.py", "name": "with space", "tags": {"with space"}},
-        {"url": "https://example3.py", "name": "with_several_tags", "tags": {"one_tag", "two_tag"}},
+        {"url": "https://example1.py", "name": "without_tag", "tags": ""},
+        {"url": "https://example2.py", "name": "with space", "tags": "with space"},
+        {"url": "https://example3.py", "name": "with_several_tags", "tags": "one_tag,two_tag"},
     ]
     assert csv_to_dicts(given) == intermediate
 
@@ -54,9 +54,9 @@ def test_add_special_items_intermediate():
 @pytest.mark.skip(reason="not implemented")
 def test_add_special_items_final():
     intermediate = [
-        {"url": "https://example1.py", "name": "without_tag", "tags": set()},
-        {"url": "https://example2.py", "name": "with space", "tags": {"with space"}},
-        {"url": "https://example3.py", "name": "with_several_tags", "tags": {"one_tag", "two_tag"}},
+        {"url": "https://example1.py", "name": "without_tag", "tags": ""},
+        {"url": "https://example2.py", "name": "with space", "tags": "with space"},
+        {"url": "https://example3.py", "name": "with_several_tags", "tags": "one_tag,two_tag"},
     ]
     expected = [
         GivenElement(url="https://example1.py", name="without_tag", tags=set()),
@@ -69,12 +69,13 @@ def test_add_special_items_final():
 def test_add_items_with_error_intermediate():
     given = """
 "https://example1.py","name1","test"
-"not_an_url","name2, "test"
+"not_an_url","name2", "test"
 "https://example3.py","name3", "python"
 """
     intermediate = [
-        {"url": "https://example1.py", "name": "name1", "tags": {"test"}},
-        {"url": "https://example3.py", "name": "name3", "tags": {"python"}},
+        {"url": "https://example1.py", "name": "name1", "tags": "test"},
+        {"url": "not_an_url", "name": "name2", "tags": "test"},
+        {"url": "https://example3.py", "name": "name3", "tags": "python"},
     ]
     assert csv_to_dicts(given) == intermediate
 
@@ -82,8 +83,9 @@ def test_add_items_with_error_intermediate():
 @pytest.mark.skip(reason="not implemented")
 def test_add_items_with_error_final():
     intermediate = [
-        {"url": "https://example1.py", "name": "name1", "tags": {"test"}},
-        {"url": "https://example3.py", "name": "name3", "tags": {"python"}},
+        {"url": "https://example1.py", "name": "name1", "tags": "test"},
+        {"url": "not_an_url", "name": "name2", "tags": "test"},
+        {"url": "https://example3.py", "name": "name3", "tags": "python"},
     ]
     expected = [
         GivenElement(url="https://example1.py", name="name1", tags={"test"}),
